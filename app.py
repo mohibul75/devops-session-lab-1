0b31eaeb2538b53100c2ede73b4ed6d3720200b1
Mohibul Alam <43554251+mohibul75@users.noreply.github.com>
2025-07-20 14:51:04 +0600

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = FastAPI()
 async def root():
     return {"message": "hello form hands on session lab 1"}
 
+@app.get("/v2/api")
+async def root():
+    return {"message": "hello api v2"}
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000) 
